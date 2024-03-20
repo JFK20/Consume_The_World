db6eb003d2908689c8ce8629d2fef4bb155322bc
j.kollien <julius.kollien@alumni.fh-aachen.de>
2024-03-20 12:36:12 +0100

    removed unnecessary code

--- Assets/Scripts/Building/OreMiner.cs
+++ Assets/Scripts/Building/OreMiner.cs
@@ -4,14 +4,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OreMiner : PlacedObject {
-    private GroundType groundType;
-    [SerializeField] private Item[] itemsToMine;
     private bool mining = false;
     public static PlacedObject Create(Vector3 WorldPosition, Vector2Int origin, PlacedObjectTypeSO.Dir dir,
         PlacedObjectTypeSO placedObjectTypeSO, GroundType groundType) {
         PlacedObject tmp = PlacedObject.Create(WorldPosition, origin, dir, placedObjectTypeSO);
         OreMiner oreMiner = tmp.GetComponent<OreMiner>();
-        oreMiner.groundType = groundType;
         return tmp;
     }
 
@@ -24,7 +21,6 @@ public class OreMiner : PlacedObject {
     }
 
     IEnumerator MineOre(int time) {
-        Item item = itemsToMine[0];
         bool suc = ItemHelper.Instance.AddItem(1, InventorySlot.IO.PrimaryOutput, ref inventorySlots);
         yield return new WaitForSeconds(time);
         mining = false;
